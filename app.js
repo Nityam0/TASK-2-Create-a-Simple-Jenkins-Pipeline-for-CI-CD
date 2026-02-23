e824a7ec93f89fbc4584176563d149b179e04009
Ubuntu <ubuntu@ip-172-31-39-46.ap-south-1.compute.internal>
2026-02-23 06:11:40 +0000

    test webhook

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write("Hello DevOps World!");
+    res.write("Hello NITYAM YOUR TASK 2 IS SUCCESSFUL COMPLETED PROCEED FOR THE NEXT ONE...... ");
     res.end();
 });
 
